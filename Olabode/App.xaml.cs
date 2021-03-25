737aefea42ede62b3c29b86b8f0efd4682a172e2
Olabode-bod <75066623+Olabode-bod@users.noreply.github.com>
2021-03-25 22:57:33 +0100

    Add files via upload

---
+++ Olabode/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Olabode
+{
+    public partial class App : Application
+    {
+        public NavigationPage AboutPage { get; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Olabode/Model/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olabode.Model
+{
+    public class Fish
+    {
+        public string State { get; set; }
+
+        public double Labour_Force_Population { get; set; }
+
+        public double Employment { get; set; }
+
+
+    }
+}
+
